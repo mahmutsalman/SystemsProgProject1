4006587535ae896f454ed6f1fad9116e5d4c2647
Mahmut <leipzig9998@hotmail.com>
2021-04-14 14:26:35 +0300

    getting inputs in txt file,1

--- main.java
+++ main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import java.lang.StringBuilder;
+import java.io.*;
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+    public static void main(String[] args) throws IOException {
+        // TODO Read txt file
+        BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\asxdc\\Desktop\\Desktop\\Projects\\Java_projects\\Systems_Programming_Project\\input.txt"));
+        String str;
+        
+        List<String> list = new ArrayList<String>();
+        while((str = in.readLine()) != null){
+            list.add(str);
+        }
+        
+        String[] stringArr = list.toArray(new String[0]);
     }
 }
