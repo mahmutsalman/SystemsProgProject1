5bdf357fd8e707fab968db45cff4d5fd9792a6cc
Mahmut <leipzig9998@hotmail.com>
2021-04-14 15:07:22 +0300

    will use list instead of an array,just to hold

---
+++ InputType.java
@@ -0,0 +1,9 @@
+public class InputType {
+    // unsigned, signed , floating point
+    private int type;
+
+    public InputType(int i){
+        this.type=i;
+    }
+
+}
--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.lang.StringBuilder;
 import java.io.*;
 class HelloWorld {
     public static void main(String[] args) throws IOException {
-        // TODO Read txt file
+        // TODO Read txt file _DONE_
         BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\asxdc\\Desktop\\Desktop\\Projects\\Java_projects\\Systems_Programming_Project\\input.txt"));
         String str;
         
@@ -13,5 +13,19 @@ class HelloWorld {
         }
         
         String[] stringArr = list.toArray(new String[0]);
+
+
+        //TODO Search for u value to understand it is unsigned integer
+
+        for (int i = 0; i < stringArr.length; i++){
+            if (stringArr[i] != null){
+                String textValue = stringArr[i];
+                for (int j = 0; j < stringArr.length; j++){
+                    if (textValue.charAt(j) == ('u')){
+                        System.out.println("found u in the element");
+                    }
+                }
+            }
+        }
     }
 }
