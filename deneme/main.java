6342540f22463461b35e22e8edbdbba1a6ad281d
Mahmut <leipzig9998@hotmail.com>
2021-04-15 22:20:17 +0300

    0

--- deneme/main.java
+++ deneme/main.java
@@ -1,84 +1,15 @@
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
- 
+
 public class main {
- 
+
     public static void main(String[] args) {
-        for ( String s : new String[] {"23.34375", ".1", "63.0"} ) {
-            String binary = decimalToBinary(new BigDecimal(s));
-            System.out.printf("%s => %s%n", s, binary);
-            System.out.printf("%s => %s%n", binary, binaryToDecimal(binary));
-        }
+
+        String textValue = "63.0";
+        textValue = textValue.substring(0, textValue.length() - 2);
+        System.out.println(textValue);
+
     }
- 
-    private static BigDecimal binaryToDecimal(String binary) {        
-        return binaryToDecimal(binary, 50);
-    }
- 
-    private static BigDecimal binaryToDecimal(String binary, int digits) {
-        int decimalPosition = binary.indexOf(".");
-        String integer = decimalPosition >= 0 ? binary.substring(0, decimalPosition) : binary;
-        String fractional = decimalPosition >= 0 ? binary.substring(decimalPosition+1) : "";
- 
-        //  Integer part
-        BigDecimal result = BigDecimal.ZERO;
-        BigDecimal powTwo = BigDecimal.ONE;
-        BigDecimal two = BigDecimal.valueOf(2);
-        for ( char c : new StringBuilder(integer).reverse().toString().toCharArray() ) {
-            result = result.add(powTwo.multiply(BigDecimal.valueOf(c - '0')));
-            powTwo = powTwo.multiply(two);
-        }
- 
-        //  Fractional part
-        MathContext mc = new MathContext(digits);
-        powTwo = BigDecimal.ONE;
-        for ( char c : fractional.toCharArray() ) {
-            powTwo = powTwo.divide(two);
-            result = result.add(powTwo.multiply(BigDecimal.valueOf(c - '0')), mc);
-        }
- 
-        return result;
-    }
- 
-    private static String decimalToBinary(BigDecimal decimal) {
-        return decimalToBinary(decimal, 50);
-    }
- 
-    private static String decimalToBinary(BigDecimal decimal, int digits) {
-        BigDecimal integer = decimal.setScale(0, RoundingMode.FLOOR);
-        BigDecimal fractional = decimal.subtract(integer);
- 
-        StringBuilder sb = new StringBuilder();
- 
-        //  Integer part
-        BigDecimal two = BigDecimal.valueOf(2);
-        BigDecimal zero = BigDecimal.ZERO;
-        while ( integer.compareTo(zero) > 0 ) {
-            BigDecimal[] result = integer.divideAndRemainder(two);
-            sb.append(result[1]);
-            integer = result[0];
-        }
-        sb.reverse();
- 
-        //  Fractional part
-        int count = 0;
-        if ( fractional.compareTo(zero) != 0 ) {
-            sb.append(".");
-        }
-        while ( fractional.compareTo(zero) != 0 ) {
-            count++;
-            fractional = fractional.multiply(two);
-            sb.append(fractional.setScale(0, RoundingMode.FLOOR));
-            if ( fractional.compareTo(BigDecimal.ONE) >= 0 ) {
-                fractional = fractional.subtract(BigDecimal.ONE);
-            }
-            if ( count >= digits ) {
-                break;
-            }
-        }
- 
-        return sb.toString();
-    }
- 
+
 }
